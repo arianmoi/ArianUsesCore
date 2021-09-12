e586fe1524dc0f84463fbed749f45aa6f90ef144
Arian <t-amoinian@microsoft.com>
2021-09-12 19:44:26 +0300

    Squashed 'abc/' content from commit 9aff2b8
    git-subtree-dir: abc
    git-subtree-split: 9aff2b80c63468113369a1801f5b7dafcedf3f35

---
+++ main.py
@@ -0,0 +1,16 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def just_changed_it():
+    # Use a breakpoint in the code line below to debug your script.
+    print('This is core repo')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    just_changed_it()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/

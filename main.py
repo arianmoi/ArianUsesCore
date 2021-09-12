cfd505e4e3ed5d31b8348f58f76809f9f6c195f8
Arian <t-amoinian@microsoft.com>
2021-09-12 20:02:00 +0300

    sgsdg

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from eee.main import just_changed_it
 
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     just_changed_it()
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':

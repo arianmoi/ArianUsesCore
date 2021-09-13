b77e4b60c3bea8125827519e47e10e3cda353a76
Arian <t-amoinian@microsoft.com>
2021-09-13 10:24:19 +0300

    delete

--- main.py
+++ main.py
@@ -2,12 +2,11 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import submodule3.main
+# import submodule3.main
 
 
 def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    submodule3.main.just_changed_it()
+    pass
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
